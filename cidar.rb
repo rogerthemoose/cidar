0c2abb1779a717f1db51dc25eb3333a3be89d7cb
Richard Filippi <richard@rogerthemoose.co.uk>
2011-11-25 00:58:42 +0100

    build a simple list based on the jenkins view

--- cidar.rb
+++ cidar.rb
@@ -1,47 +1,92 @@
 require 'nokogiri'
 require 'open-uri'
-require 'sinatra' 
+require 'sinatra'
 require 'json'
 
-JENKINS_URL = 'http://ci.lhotse.ov.otto.de:8080/view/p13n/job/'
+URL = 'http://ci.lhotse.ov.otto.de:8080/view/'
 
 get '/' do
   erb :index
 end
 
+get '/p13n' do
+  erb :p13n
+end
+
 helpers do
-  def status_of(project)
-    summary = JSON.parse(open(JENKINS_URL + project + '/api/json').read)
-    detail = JSON.parse(open(JENKINS_URL + project + '/lastBuild/api/json').read)
-    @status = Status.new(summary, detail)
-    erb 'status <%= if @status.success? then "success" else "failure" end %><%= " building" if @status.building? %>'
+  def status_for(project)
+    @info = Info.new(project)
+    erb 'status <%= if @info.success? then "success" else "failure" end %><%= " building" if @info.building? %>'
   end
+end
+
+class Info
+  def initialize(project)
+    @project = project
+    @view = JSON.parse(open(URL + project + '/api/json').read)
+  end
+
+  def name
+    @view['name']
+  end
+
+  def description
+    @view['description']
+  end
+
+  def jobs
+    @view['jobs'].collect { |job| Job.new(job['url']) }
+  end
+
+  def success?
+    jobs.inject(true) { |result, job| result && job.success? }
+  end
+
+  def building?
+    jobs.inject(false) { |result, job| result || job.building? }
+  end
+
 
 end
 
-class Status
-  def initialize(summary, detail)
-    @summary = summary
-    @detail = detail
+class Job
+  def initialize(url)
+    @job = JSON.parse(open(url + '/api/json').read)
+    @detail = JSON.parse(open(url + '/lastBuild/api/json').read)
   end
-  
+
+  def name
+    @job['name']
+  end
+
+  def health
+    stability = @job['healthReport'].select { |entry| entry ['description'] =~ /Build stability/ }
+    stability && stability.last()['score']
+  end
+
+  def buildLabel
+    buildNumber = @job['lastBuild'] && @job['lastBuild']['number']
+    '#' + buildNumber.to_s + ' ' + (commiter_names() && commiter_names().first()).to_s
+  end
+
+  def status?
+    'status' + (success? ? ' success' : ' failure ') + (building? ? ' building' : '')
+  end
+
   def success?
     @detail['result'] == "SUCCESS"
   end
-  
+
   def building?
     @detail['building']
   end
-  
-  def buildLabel
-    buildNumber = @summary['lastBuild'] && @summary['lastBuild']['number']
-    '#' + buildNumber.to_s + ' ' + (commiter_names() && commiter_names().first()).to_s
-  end
-  
+
   def commit_message
-    @detail['changeSet']['items'].last()['comment']
+    if (!@detail['changeSet']['items'].empty?)
+      @detail['changeSet']['items'].last()['comment']
+    end
   end
-  
+
   def commiters
     knownNames = commiter_names & knownPeople
     if (!(commiter_names - knownNames).empty?)
@@ -50,15 +95,10 @@ class Status
     knownNames
   end
 
-  def health
-    stability = @summary['healthReport'].select {|entry| entry ['description'] =~ /Build stability/}
-    stability && stability.last()['score']
-  end
-
   private
 
   def commiter_names()
-    @detail['changeSet']['items'].collect{|commit| commit['author']['fullName']}.uniq
+    @detail['changeSet']['items'].collect { |commit| commit['author']['fullName'] }.uniq
   end
 
   def knownPeople()
